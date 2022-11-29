fa3e46bac5e8e6d41e8157f9b3b9bfeee19258e1
Abdullah Al Hadi <abdullahalhadi317@gmail.com>
2022-11-29 16:53:05 +0600

    button ripple

---
+++ app.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('.btn');
+
+button.addEventListener("mouseover", (e)=> {
+    const x = (e.pageX - button.offsetLeft);
+    const y = (e.pageY - button.offsetTop);
+
+    button.style.setProperty("--xPos", x + "px");
+    button.style.setProperty("--yPos", y + "px");
+})
+
